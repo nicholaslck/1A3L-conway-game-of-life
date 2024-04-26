7a6ded432b09f7421e5bcc66ff83abeea4660fe9
Nicholas Lau <nicholaslck@gmail.com>
2024-04-26 23:15:02 +0800

    fix lib dependencies

--- apps/svelte/src/lib/Counter.svelte
+++ apps/svelte/src/lib/Counter.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
+  import { core } from "core"
+  
   let count: number = 0;
   const increment = () => {
     count += 1;
   };
+
+
 </script>
 
 <button on:click={increment}>
   count is {count}
 </button>
+
+<div>{ core() }</div>
